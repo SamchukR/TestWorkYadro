486e899a6954432004e9f04501c8e7d93a0cea57
root <root@roman.domain.com>
2023-09-23 19:51:40 +0300

    Add runtime dependense

--- source/deb/files/start.sh
+++ source/deb/files/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cp stdeb.cfg prometheus_flask_exporter-0.22.4/
 cd ./prometheus_flask_exporter-0.22.4
 python3 setup.py --command-packages=stdeb.command bdist_deb
 cp -r ./deb_dist/*.deb /Module/files/packet/
--- source/rpm/files/start.sh
+++ source/rpm/files/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ./prometheus_flask_exporter-0.22.4
-fpm --python-bin=python3 -s python -t rpm setup.py
+fpm --python-bin=python3 -s python -t rpm --python-package-name-prefix python3 --depends "python3 >= 3.6" --name prometheus-flask-exporter-pkg-rpm setup.py
 cp -r ./*.rpm /Module/files/packet/
